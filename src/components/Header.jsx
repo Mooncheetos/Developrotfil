5315f813f98b813c1cdcb1edb6bf6debb6177fae
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-11-29 00:37:49 +0200

    update Header
    rework

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ function Header() {
       <nav className="nav">
         <Link to="/" className="nav-link">Головна</Link>
         <span className="nav-divider">|</span>
-        <Link to="/upload" className="nav-link">Графік</Link>
+        <Link to="/upload" className="nav-link">Графік хвилі й розрахунок товщини</Link>
         <span className="nav-divider">|</span>
-        <Link to="/thickness" className="nav-link">Розрахунок товщини</Link>
+        {/* <Link to="/thickness" className="nav-link">Розрахунок товщини</Link>
         <span className="nav-divider">|</span>
         <Link to="/bandgap" className="nav-link">Ширина забороненої зони</Link>
         <span className="nav-divider">|</span>
-        <Link to="/refractive-index" className="nav-link">Коэффициент преломления</Link>
+        <Link to="/refractive-index" className="nav-link">Коэффициент преломления</Link> */}
       </nav>
     </header>
   );
